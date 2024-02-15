481a5174f489bc3245296b0cb4af3eef92bcdcf4
Pradeep <nspradeep53@gmail.com>
2024-02-15 09:36:02 +0530

    Added New LWC Component

---
+++ force-app/main/default/lwc/helloWorldJestTest/helloWorldJestTest.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorldJestTest extends LightningElement {
+    message = 'JEST Test!';
+
+    handleChange(event) {
+        this.message = event.target.value;
+    }
+}
